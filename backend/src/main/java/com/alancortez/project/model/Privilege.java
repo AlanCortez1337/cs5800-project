730cf4026e2ab571ebe8139146d701cb63623bb0
Alan Cortez <acortez1337@csu.fullerton.edu>
2025-11-16 20:24:31 -0800

    create Privilege builder class

---
+++ backend/src/main/java/com/alancortez/project/model/Privilege.java
@@ -0,0 +1,160 @@
+package com.alancortez.project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Embeddable // Marks the class as embeddable in other entities
+@NoArgsConstructor // Lombok: Generates a no-argument constructor
+@AllArgsConstructor // Lombok: Generates a constructor with all fields
+public class Privilege {
+    @Column(name = "can_create_ingredient", nullable = false)
+    private Boolean canCreateIngredient = false;
+
+    @Column(name = "can_read_ingredient", nullable = false)
+    private Boolean canReadIngredient = false;
+
+    @Column(name = "can_update_ingredient", nullable = false)
+    private Boolean canUpdateIngredient = false;
+
+    @Column(name = "can_delete_ingredient", nullable = false)
+    private Boolean canDeleteIngredient = false;
+
+    @Column(name = "can_create_recipe", nullable = false)
+    private Boolean canCreateRecipe = false;
+
+    @Column(name = "can_read_recipe", nullable = false)
+    private Boolean canReadRecipe = false;
+
+    @Column(name = "can_update_recipe", nullable = false)
+    private Boolean canUpdateRecipe = false;
+
+    @Column(name = "can_delete_recipe", nullable = false)
+    private Boolean canDeleteRecipe = false;
+
+    private Privilege(PrivilegeBuilder builder) {
+        canCreateIngredient = builder.canCreateIngredient;
+        canReadIngredient = builder.canReadIngredient;
+        canUpdateIngredient = builder.canUpdateIngredient;
+        canDeleteIngredient = builder.canDeleteIngredient;
+        canCreateRecipe = builder.canCreateRecipe;
+        canReadRecipe = builder.canReadRecipe;
+        canUpdateRecipe = builder.canUpdateRecipe;
+        canDeleteRecipe = builder.canDeleteRecipe;
+    }
+
+    public static class PrivilegeBuilder {
+        private Boolean canCreateIngredient;
+        private Boolean canReadIngredient;
+        private Boolean canUpdateIngredient;
+        private Boolean canDeleteIngredient;
+        private Boolean canCreateRecipe;
+        private Boolean canReadRecipe;
+        private Boolean canUpdateRecipe;
+        private Boolean canDeleteRecipe;
+
+        public PrivilegeBuilder() {}
+
+        public PrivilegeBuilder allowCreateIngredient() {
+            this.canCreateIngredient = true;
+            return this;
+        }
+        public PrivilegeBuilder allowReadIngredient() {
+            this.canReadIngredient = true;
+            return this;
+        }
+        public PrivilegeBuilder allowUpdateIngredient() {
+            this.canUpdateIngredient = true;
+            return this;
+        }
+        public PrivilegeBuilder allowDeleteIngredient() {
+            this.canDeleteIngredient = true;
+            return this;
+        }
+        public PrivilegeBuilder allowCreateRecipe() {
+            this.canCreateRecipe = true;
+            return this;
+        }
+        public PrivilegeBuilder allowReadRecipe() {
+            this.canReadRecipe = true;
+            return this;
+        }
+        public PrivilegeBuilder allowUpdateRecipe() {
+            this.canUpdateRecipe = true;
+            return this;
+        }
+        public PrivilegeBuilder allowDeleteRecipe() {
+            this.canDeleteRecipe = true;
+            return this;
+        }
+
+        public Privilege build() {
+            return new Privilege(this);
+        }
+    }
+
+    public Boolean getCanCreateIngredient() {
+        return canCreateIngredient;
+    }
+
+    public Boolean getCanReadIngredient() {
+        return canReadIngredient;
+    }
+
+    public Boolean getCanUpdateIngredient() {
+        return canUpdateIngredient;
+    }
+
+    public Boolean getCanDeleteIngredient() {
+        return canDeleteIngredient;
+    }
+
+    public Boolean getCanCreateRecipe() {
+        return canCreateRecipe;
+    }
+
+    public Boolean getCanReadRecipe() {
+        return canReadRecipe;
+    }
+
+    public Boolean getCanUpdateRecipe() {
+        return canUpdateRecipe;
+    }
+
+    public Boolean getCanDeleteRecipe() {
+        return canDeleteRecipe;
+    }
+
+    public void setCanCreateIngredient(Boolean canCreateIngredient) {
+        this.canCreateIngredient = canCreateIngredient;
+    }
+
+    public void setCanReadIngredient(Boolean canReadIngredient) {
+        this.canReadIngredient = canReadIngredient;
+    }
+
+    public void setCanUpdateIngredient(Boolean canUpdateIngredient) {
+        this.canUpdateIngredient = canUpdateIngredient;
+    }
+
+    public void setCanDeleteIngredient(Boolean canDeleteIngredient) {
+        this.canDeleteIngredient = canDeleteIngredient;
+    }
+
+    public void setCanCreateRecipe(Boolean canCreateRecipe) {
+        this.canCreateRecipe = canCreateRecipe;
+    }
+
+    public void setCanReadRecipe(Boolean canReadRecipe) {
+        this.canReadRecipe = canReadRecipe;
+    }
+
+    public void setCanUpdateRecipe(Boolean canUpdateRecipe) {
+        this.canUpdateRecipe = canUpdateRecipe;
+    }
+
+    public void setCanDeleteRecipe(Boolean canDeleteRecipe) {
+        this.canDeleteRecipe = canDeleteRecipe;
+    }
+}
