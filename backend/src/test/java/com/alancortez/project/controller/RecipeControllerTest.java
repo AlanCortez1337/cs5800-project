bffcac5d4d48c3fcf8a62bc54cb6b94364fcdb2e
Alan <cortez.alan060@gmail.com>
2025-11-22 20:16:40 -0800

    minor backend fixes

--- backend/src/test/java/com/alancortez/project/controller/RecipeControllerTest.java
+++ backend/src/test/java/com/alancortez/project/controller/RecipeControllerTest.java
@@ -24,7 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class RecipeControllerTest {
+public class RecipeControllerTest {
 
     @Mock
     private RecipeService recipeService;
--- backend/src/test/java/com/alancortez/project/controller/UserControllerTest.java
+++ backend/src/test/java/com/alancortez/project/controller/UserControllerTest.java
@@ -22,7 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class UserControllerTest {
+public class UserControllerTest {
 
     @Mock
     private UserService userService;
